465547e45f04f87c44b66834aacf21a7d77a1a6a
jacklmbrt07 <jacklmbrt07@gmail.com>
2020-11-23 18:44:30 -0800

    started react-map-gl

---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,10 @@
+import { Navbar } from "react-bootstrap";
+
+const NavBar = (props) => (
+  <div>
+    {" "}
+    <Navbar bg="light" expand="lg">hello world</Navbar>
+  </div>
+);
+
+export default NavBar;
