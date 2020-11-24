7f2ec1c5b717f5b7c7a6be5eb408516c0b8e12f9
Minkaron <aaron.tucaz@gmail.com>
2020-11-23 22:05:47 -0700

    Modifying routes for authentication

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import './App.css';
 
+import { Switch, Route } from 'react-router-dom';
+
 import NavBar from '../../components/NavBar/NavBar';
 import Footer from '../../components/Footer/Footer';
+
+import Landing from '../../pages/Landing/Landing';
+import Login from '../../pages/Login/Login';
+import Dashboard from '../../pages/Dashboard/Dashboard';
 
 class App extends React.Component {
   constructor() {
@@ -13,6 +19,17 @@ class App extends React.Component {
     return (
       <div className='App'>
         <NavBar />
+        <Switch>
+          <Route path='/login'>
+            <Login />
+          </Route>
+          <Route path='/admin'>
+            <Dashboard />
+          </Route>
+          <Route path='/'>
+            <Landing />
+          </Route>
+        </Switch>
         <Footer />
       </div>
     );
